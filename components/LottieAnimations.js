a544acb6584ee15841df0bddd2e3087b2351a3ba
Arup Das <arupdas@Arups-MacBook-Air.local>
2022-05-05 09:15:09 +0200

    loop lottie

--- components/LottieAnimations.js
+++ components/LottieAnimations.js
@@ -5,7 +5,7 @@ import LottieView from "lottie-react-native";
 export default function LottieAnimations() {
   return (
     <View style={{ width: 500, height: 500 }}>
-      <LottieView source={require("../assets/doggy.json")} autoPlay />
+      <LottieView source={require("../assets/doggy.json")} autoPlay loop />
     </View>
   );
 }
